2950ce9934c773c18618c6137d225d54eccb5aae
solahn <solahn00@gmail.com>
2023-10-09 15:27:32 +0900

    test.sh -model {your-model-name}

--- sequential_test.sh
+++ sequential_test.sh
@@ -54,8 +54,5 @@ else
     exit 1
 fi
 
-# GPU-accelerated with optimal_core
-for var in $(seq 1 $layer_num)
-do
-    ./darknet detector gpu-accel ./cfg/${data_file}.data ./cfg/${model}.cfg ./weights/${model}.weights data/dog.jpg -num_thread 1 -glayer $var -num_exp 30
-done
+# Sequential
+./darknet detector sequential ./cfg/${data_file}.data ./cfg/${model}.cfg ./weights/${model}.weights data/dog.jpg -core_id 3 -num_exp 40
--- src/parser.c
+++ src/parser.c
@@ -997,7 +997,7 @@ layer parse_shortcut(list *options, size_params params, network net)
 
     for (i = 0; i < n; ++i) {
         int index = layers[i];
-        assert(params.w == net.layers[index].out_w && params.h == net.layers[index].out_h);
+        // assert(params.w == net.layers[index].out_w && params.h == net.layers[index].out_h);
 
         if (params.w != net.layers[index].out_w || params.h != net.layers[index].out_h || params.c != net.layers[index].out_c)
             fprintf(stderr, " (%4d x%4d x%4d) + (%4d x%4d x%4d) \n",
