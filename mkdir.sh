31385985448ffb331f32367193986cc53590cde7
HaileeKim <0206dbsh@gmail.com>
2023-10-12 18:58:14 +0900

    add gpu-accel-reverse.c

--- mkdir.sh
+++ mkdir.sh
@@ -78,6 +78,12 @@ do
 	mkdir -p gpu-accel/$model/
 done
 
+## GPU-Accel
+for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
+do
+	mkdir -p gpu-accel-reverse/$model/
+done
+
 ## GPU-Accel_1thread
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
 do
--- src/detector.h
+++ src/detector.h
@@ -34,6 +34,9 @@ void data_parallel_mp(char *datacfg, char *cfgfile, char *weightfile, char *file
 void gpu_accel(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh,
     float hier_thresh, int dont_show, int theoretical_exp, int ext_output, int save_labels, char *outfile, int letter_box, int benchmark_layers);
 
+void gpu_accel_reverse(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh,
+    float hier_thresh, int dont_show, int theoretical_exp, int ext_output, int save_labels, char *outfile, int letter_box, int benchmark_layers);
+
 void gpu_accel_mp(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh,
     float hier_thresh, int dont_show, int ext_output, int save_labels, char *outfile, int letter_box, int benchmark_layers);
 
