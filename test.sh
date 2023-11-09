b0946c37a91d2b97ad9b2f58054ac3ef882fd2e9
solahn <solahn00@gmail.com>
2023-11-09 21:43:18 +0900

    add test mode

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 ./gpu_accel_gpu_test.sh -model densenet201
 ./gpu_accel_gpu_pre_test.sh -model densenet201
+
+# ./layer_time_test.sh -model densenet201
+# ./sequential_test.sh -model densenet201
+# ./data_parallel_test.sh -model densenet201
+# ./pipeline_test.sh -model densenet201
+# ./gpu_accel_test.sh -model densenet201
