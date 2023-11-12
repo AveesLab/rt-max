920a16db931bcc888ba7e2fb7f709ec0dcbf0baa
solahn <solahn00@gmail.com>
2023-11-12 12:42:19 +0900

    minor fix

--- test.sh
+++ test.sh
@@ -1,19 +1,26 @@
-./gpu_accel_pre_gpu_test.sh -model yolov4
-./gpu_accel_gpu_test.sh -model yolov4
-./gpu_accel_test.sh -model yolov4
+# ./gpu_accel_pre_gpu_test.sh -model densenet201
+# ./gpu_accel_gpu_test.sh -model densenet201
+# ./gpu_accel_test.sh -model densenet201
 
 
-sleep 3
-./sequential_test.sh -model yolov4
-sleep 3
-./pipeline_test.sh -model yolov4
-sleep 3
-./data_parallel_test.sh -model yolov4
-sleep 3
+# sleep 3
+# ./sequential_test.sh -model densenet201
+# sleep 3
+# ./pipeline_test.sh -model densenet201
+# sleep 3
 
-./data_parallel_jitter_test.sh -model yolov4
+# sleep 3
+# ./data_parallel_test.sh -model densenet201
+# sleep 3
+# ./data_parallel_jitter_test.sh -model densenet201
+# sleep 3
+# ./data_parallel_sleep_test.sh -model densenet201
+# sleep 3
+
+
+./data_parallel_r_test_test.sh -model densenet201
 sleep 3
-./data_parallel_sleep_test.sh -model yolov4
+./data_parallel_r_test_jitter_test.sh -model densenet201
 sleep 3
 
 
