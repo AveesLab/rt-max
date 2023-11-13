7bed576e6e21bea498cc3f1ae7a894e128caf304
HaileeKim <0206dbsh@gmail.com>
2023-11-14 03:28:34 +0900

    Energy consumption per 1ms

--- energy_consum.sh
+++ energy_consum.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-output_file="cpu_energy_usage.csv"  # 출력 파일명
+output_file="cpu_energy_usage_data-parallel.csv"  # 출력 파일명
 
 # CSV 파일 헤더 작성
 echo "Timestamp,CPU Power Consumption (mW)" > "$output_file"
 
-# tegrastats 실행 및 CPU 전력 소모량 추출
-(stdbuf -oL sudo tegrastats --interval 1000 | while read line; do
+# tegrastats 실행 및 CPU 전력 소모량 추출 (Per 1ms : --interval 1)
+(sudo tegrastats --interval 1 | while read line; do
+  timestamp=$(date +%s%3N)  # 밀리초 단위 타임스탬프
   cpu_power=$(echo $line | grep -oP 'VDD_CPU_CV \K\d+mW')  # CPU 전력 소모량 추출
-
-  if [ ! -z "$cpu_power" ]; then
-    timestamp=$(date +%s%3N)  # 밀리초 단위 타임스탬프
-    echo "$timestamp,$cpu_power" >> "$output_file"
-  fi
+  
+  # CSV 파일에 측정값 추가
+  echo "$timestamp,$cpu_power" >> "$output_file"
 done) &
-
-
 
 # 배경에서 tegrastats를 실행하기 위해 프로세스 ID를 저장합니다.
 TEGRASTATS_PID=$!
 
 # 여기에 다른 명령이나 스크립트를 실행할 수 있습니다.
-./sequential_test.sh -model densenet201
+./data_parallel_test.sh -model densenet201
 
 # 스크립트 실행이 완료되면 tegrastats 프로세스 종료
 kill $TEGRASTATS_PID
