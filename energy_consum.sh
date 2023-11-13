51033d00ab63cdeff11d8ff8fa9eee2b1af73f16
solahn <61405226+solahn@users.noreply.github.com>
2023-11-13 15:32:54 +0900

    Update energy_consum.sh

--- energy_consum.sh
+++ energy_consum.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-# 추론 스크립트 파일 경로 설정
-INFER_SCRIPT="sequential_test.sh -model densenet201"
-
-# CSV 로그 파일 경로 설정
-CSV_LOG="cpu_energy_log.csv"
+output_file="system_energy_usage.csv"  # 출력 파일명
 
 # CSV 파일 헤더 작성
-echo "Time, CPU Energy" > "$CSV_LOG"
+echo "Timestamp,Total Energy Consumption (W)" > "$output_file"
 
-# tegrastats를 사용하여 에너지 사용량 모니터링 (초당)
+# tegrastats 실행 및 에너지 사용량 추출
 (sudo tegrastats --interval 1000 | while read line; do
-    # 현재 시간과 CPU 에너지 사용량 추출
-    TIMESTAMP=$(date +"%T.%3N")
-    CPU_ENERGY=$(echo $line | grep -oP 'CPU \K\d+')
-
-    # CSV 파일에 기록
-    echo "$TIMESTAMP, $CPU_ENERGY" >> "$CSV_LOG"
+  timestamp=$(date +%s%3N)  # 밀리초 단위 타임스탬프
+  total_power=$(echo $line | grep -oP 'POM_5V_IN \K\d+' | awk '{print $1}')  # 시스템 전체 전력 소모량 추출
+  
+  # CSV 파일에 측정값 추가
+  echo "$timestamp,$total_power" >> "$output_file"
 done) &
 
-# tegrastats 프로세스 ID 저장
+# 배경에서 tegrastats를 실행하기 위해 프로세스 ID를 저장합니다.
 TEGRASTATS_PID=$!
 
-# DNN 추론 실행
-bash "$INFER_SCRIPT"
+# 여기에 다른 명령이나 스크립트를 실행할 수 있습니다.
+# 예: bash "$INFER_SCRIPT"
 
-# 추론이 끝나면 tegrastats 종료
+# 스크립트 실행이 완료되면 tegrastats 프로세스 종료
 kill $TEGRASTATS_PID
 
-echo "DNN 추론 완료. 에너지 사용량은 $CSV_LOG 파일에 기록되었습니다."
+echo "모니터링 완료. 에너지 사용량은 $output_file 파일에 기록되었습니다."
