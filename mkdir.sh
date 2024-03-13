ddfd87cfbca85badaa07a9404a589d67ffe2dbbb
solahn <solahn00@gmail.com>
2024-03-13 19:56:43 +0900

    Edit mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -13,50 +13,16 @@ do
 	mkdir -p sequential/$model/
 done
 
-touch sequential_cpu_utilization.csv
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	cp sequential_cpu_utilization.csv sequential/$model/
-done
-rm sequential_cpu_utilization.csv
-
 ## Sequential-multiblas
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
 do
 	mkdir -p sequential-multiblas/$model/
 done
 
-touch sequential_cpu_utilization.csv
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	cp sequential_cpu_utilization.csv sequential-multiblas/$model/
-done
-rm sequential_cpu_utilization.csv
-
 ## Pipeline
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
 do
 	mkdir -p pipeline/$model/
-done
-
-touch pipeline_cpu_utilization.csv
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	cp pipeline_cpu_utilization.csv pipeline/$model/
-done
-rm pipeline_cpu_utilization.csv
-
-
-## Data-Parallel
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p data-parallel_r_test/$model/
-done
-
-## Data-Parallel
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p data-parallel_r_test_jitter/$model/
 done
 
 
@@ -66,25 +32,12 @@ do
 	mkdir -p data-parallel/$model/
 done
 
-touch data-parallel_cpu_utilization.csv
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	cp data-parallel_cpu_utilization.csv data-parallel/$model/
-done
-rm data-parallel_cpu_utilization.csv
 
 ## Data-Parallel-MP
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
 do
 	mkdir -p data-parallel-mp/$model/
 done
-
-touch data-parallel-mp_cpu_utilization.csv
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	cp data-parallel-mp_cpu_utilization.csv data-parallel-mp/$model/
-done
-rm data-parallel-mp_cpu_utilization.csv
 
 ## GPU-Accel
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
@@ -95,26 +48,7 @@ done
 ## GPU-Accel
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
 do
-	mkdir -p gpu-accel_sleep/$model/
-done
-
-## GPU-Accel
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel_pre_gpu/$model/
-done
-
-## GPU-Accel
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel_gpu/$model/
-done
-
-
-## GPU-Accel
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel-reverse/$model/
+	mkdir -p gpu-accel_jitter/$model/
 done
 
 ## GPU-Accel_1thread
@@ -122,25 +56,6 @@ for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet
 do
 	mkdir -p gpu-accel_1thread/$model/
 done
-
-## GPU-Accel_multi-thread
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel_multi-thread/$model/
-done
-
-## GPU-Accel_1thread
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel-reverse_1thread/$model/
-done
-
-## GPU-Accel_multi-thread
-for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
-do
-	mkdir -p gpu-accel-reverse_multi-thread/$model/
-done
-
 
 ## GPU-Accel-MP
 for model in "yolov4" "yolov4-tiny" "yolov7" "yolov7-tiny" "densenet201" "resnet152" "csmobilenet-v2" "squeezenet" "enetb0"
@@ -193,54 +108,6 @@ for var in {1..138}
 	do
 	mkdir -p cpu-reclaiming/enetb0/${var}glayer/
 	done
-
-
-## CPU-Reclaiming-Reverse
-for var in {1..305}
-	do
-	mkdir -p cpu-reclaiming-reverse/densenet201/${var}glayer/
-	done
-
-for var in {1..37}
-	do
-	mkdir -p cpu-reclaiming-reverse/yolov4-tiny/${var}glayer/
-	done
-
-for var in {1..162}
-	do
-	mkdir -p cpu-reclaiming-reverse/yolov4/${var}glayer/
-	done
-
-for var in {1..100}
-	do
-	mkdir -p cpu-reclaiming-reverse/yolov7-tiny/${var}glayer/
-	done
-
-for var in {1..144}
-	do
-	mkdir -p cpu-reclaiming-reverse/yolov7/${var}glayer/
-	done
-
-for var in {1..207}
-	do
-	mkdir -p cpu-reclaiming-reverse/resnet152/${var}glayer/
-	done
-
-for var in {1..81}
-	do
-	mkdir -p cpu-reclaiming-reverse/csmobilenet/${var}glayer/
-	done
-
-for var in {1..51}
-	do
-	mkdir -p cpu-reclaiming-reverse/squeezenet/${var}glayer/
-	done
-
-for var in {1..138}
-	do
-	mkdir -p cpu-reclaiming-reverse/enetb0/${var}glayer/
-	done
-
 
 ## CPU-Reclaiming-mp
 for var in {1..305}
