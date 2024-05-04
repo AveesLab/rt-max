21247200179cdaad5ad1b3f4d2b46b89bd17ee92
solahn <solahn00@gmail.com>
2024-05-05 08:59:52 +0900

    reset wcet_ratio = 1.03 in cpu-reclaiming-GRC.c

--- test.sh
+++ test.sh
@@ -1,18 +1,4 @@
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 1 -num_exp 30
-
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 59 -num_exp 30
-
-
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 82 -num_exp 30
-
-
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 128 -num_exp 30
-
-
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 142 -num_exp 30
-
-
-./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 150 -num_exp 30
+./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 165 -num_exp 30
 
 
 ./darknet detector cpu-reclaiming ./cfg/imagenet1k.data ./cfg/densenet201.cfg ./weights/densenet201.weights data/dog.jpg -num_thread 11 -glayer 152 -num_exp 30
