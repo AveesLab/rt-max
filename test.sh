ae6a7901085e7e1789dc4e925c3b3463352293e2
solahn <solahn00@gmail.com>
2024-05-11 18:27:07 +0900

    minor fix

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 11 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 10 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 9 -clean
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 11
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 10
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 9
 
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 8 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 7 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 6 -clean
 
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 5 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 4 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 3 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 2 -clean
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 1 -clean
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 8
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 7
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 6
+
+
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 5
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 4
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 3
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 2
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 1
