1875c646457776cd094fccb21672ad96ea797529
solahn <solahn00@gmail.com>
2024-05-15 07:24:46 +0900

    test.sh for desktop-exp

--- test.sh
+++ test.sh
@@ -1,55 +1,12 @@
 # ORIN 1
+./gpu_accel_GC_test.sh -model densenet201 -num_thread 14
+./gpu_accel_GC_test.sh -model densenet201 -num_thread 13
+./gpu_accel_GC_test.sh -model densenet201 -num_thread 12
 ./gpu_accel_GC_test.sh -model densenet201 -num_thread 11
 ./gpu_accel_GC_test.sh -model densenet201 -num_thread 10
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 9
 
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 8
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 7
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 6
-
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 5
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 4
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 3
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 2
-./gpu_accel_GC_test.sh -model densenet201 -num_thread 1
-
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 14
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 13
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 12
+./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 11
 ./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 10
-./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 9
-./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 8
-./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 7
-./cpu_reclaiming_GRC_test.sh -model densenet201 -num_thread 6
-
-# ORIN 2
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 11
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 10
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 9
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 8
-
-./cpu_reclaiming_GRC_test.sh -model resnet152 -num_thread 10
-./cpu_reclaiming_GRC_test.sh -model resnet152 -num_thread 9
-./cpu_reclaiming_GRC_test.sh -model resnet152 -num_thread 8
-
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 7
-./cpu_reclaiming_GRC_test.sh -model resnet152 -num_thread 7
-
-./gpu_accel_GC_test.sh -model resnet152 -num_thread 6
-./cpu_reclaiming_GRC_test.sh -model resnet152 -num_thread 6
-
-# ORIN 3
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 11
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 10
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 9
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 8
-
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 10
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 9
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 8
-
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 7
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 7
-
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 6
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 6
-
-./gpu_accel_GC_test.sh -model csmobilenet-v2 -num_thread 5
-./cpu_reclaiming_GRC_test.sh -model csmobilenet-v2 -num_thread 5
